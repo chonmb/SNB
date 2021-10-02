07f0b6753b2a8b58145e0aa7aef9e8a38d068655
root <root@DESKTOP-08QSDPF.localdomain>
2021-10-02 08:45:29 +0800

    add some simple validation for network.json

--- src/manager/SManager.py
+++ src/manager/SManager.py
@@ -7,7 +7,7 @@ import json
 from src.spreader.SLan import SLan
 from src.nodes.SClient import SClient
 from src.nodes.SBridge import SBridge
-
+from src.utils.Util import Util
 
 def parse_network_config():
     with open("resource/network.json", encoding='utf-8') as network:
@@ -22,13 +22,14 @@ class SManager:
         self.lans = {}
         self.frames = {}
         self.network = parse_network_config()
+        self.check_network()
         self.init_network()
 
     def init_network(self):
         """
         load network from network.json(it is the default network)
         """
-        for lan in self.network["lan"]:
+        for lan in self.network["lans"]:
             self.lans[lan["name"]] = SLan(lan["name"])
         for client in self.network["clients"]:
             c = SClient(client["name"], client["mac"])
@@ -39,6 +40,39 @@ class SManager:
             self.bridges[bridge["name"]] = b
             for lan_name in bridge["lans"]:
                 self.lans[lan_name].add_node(b)
+
+    def check_network(self):
+        """
+        check some basic network config errors such as repeated client name.
+        """ 
+        bridge_names = [bridge['name'] for bridge in self.network["bridges"]]
+        res = Util.check_repeated_elements(bridge_names)
+        if res is not None:
+            self.init_failed(res + " already existed!")
+
+        lan_names = [lan['name'] for lan in self.network["lans"]]
+        res = Util.check_repeated_elements(lan_names)
+        if res is not None:
+            self.init_failed(res + " already existed!")
+        
+        client_names = [client['name'] for client in self.network["clients"]]
+        res = Util.check_repeated_elements(client_names)
+        if res is not None:
+            self.init_failed(res + " already existed!")
+
+        for bridge in self.network["bridges"]:
+            for lan_name in bridge["lans"]:
+                if lan_name not in lan_names:
+                    self.init_failed(lan_name+' is not defiend')
+
+        for client in self.network["clients"]:
+            if client["lan"] not in lan_names:
+                    self.init_failed(client["lan"]+' is not defiend')
+
+    def init_failed(self,reason):
+        error_prefix = 'snb quit because of incorrect network config:'
+        print(error_prefix+reason)
+        exit(0)
 
     def clear_network(self):
         self.frames.clear()
